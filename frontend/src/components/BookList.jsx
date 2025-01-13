cde52b43a662e1fb60311a08a5bdc1db96b21f86
Ezeigbo Emmanuel <ezeigboemmanuel2@gmail.com>
2025-01-14 00:17:40 +0100

    Footer done

--- frontend/src/components/BookList.jsx
+++ frontend/src/components/BookList.jsx
@@ -1,7 +1,7 @@
 const BookList = () => {
   const booklist = ["", "", "", "", "", ""];
   return (
-    <div className="text-[#252422] bg-[#CCC5B9] px-4 md:px-12 pb-10">
+    <div className="text-[#252422] bg-[#CCC5B9] px-4 md:px-12 pb-20">
       <h1 className="py-6 text-lg md:text-2xl lg:text-3xl w-full mx-auto max-w-6xl">Reader&rsquo;s favorites</h1>
 
       <div className="grid grid-cols-2 md:grid-cols-4 gap-3 lg:gap-6 w-full mx-auto max-w-6xl">
--- frontend/src/pages/Homepage.jsx
+++ frontend/src/pages/Homepage.jsx
@@ -1,4 +1,5 @@
 import BookList from "../components/BookList";
+import Footer from "../components/Footer";
 import Hero from "../components/Hero";
 import Navbar from "../components/Navbar";
 
@@ -8,6 +9,7 @@ const Homepage = () => {
       <Navbar />
       <Hero />
       <BookList />
+      <Footer />
     </>
   );
 };
