b60b328f3a5ed1f5e54905999c1ea4ed6d23dffb
Ezeigbo Emmanuel <ezeigboemmanuel2@gmail.com>
2025-01-14 11:47:26 +0100

    Login design done

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,7 @@ import Homepage from "./pages/Homepage";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import SignUp from "./pages/SignUp";
+import LogIn from "./pages/LogIn";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Homepage />} />
         <Route path="/signup" element={<SignUp />} />
+        <Route path="/login" element={<LogIn />} />
       </Routes>
       <Footer />
     </>
--- frontend/src/pages/LogIn.jsx
+++ frontend/src/pages/LogIn.jsx
@@ -1,8 +1,8 @@
-const SignUp = () => {
+const LogIn = () => {
   return (
     <div className="min-h-screen text-[#252422] bg-[#CCC5B9] px-4 md:px-12">
       <h2 className="text-center font-semibold pt-8 md:text-2xl w-full max-w-xl mx-auto">
-        Sign Up
+        Log In
       </h2>
 
       <div className="w-full max-w-xl mx-auto flex flex-col justify-center items-center space-y-4 mt-10">
@@ -22,19 +22,13 @@ const SignUp = () => {
             className="w-full px-3 py-1.5 md:py-2 text-[#252422] rounded-lg bg-[#FFFCF2]"
           />
         </div>
-        <div className="flex flex-col w-full">
-          <label className="md:text-lg">Confirm Password:</label>
-          <input
-            type="password"
-            placeholder="Confirm your password"
-            className="w-full px-3 py-1.5 md:py-2 text-[#252422] rounded-lg bg-[#FFFCF2]"
-          />
-        </div>
 
-        <button className="w-full bg-[#403D39] text-[#FFFCF2] py-2 font-medium rounded-lg">Sign up</button>
+        <button className="w-full bg-[#403D39] text-[#FFFCF2] py-2 font-medium rounded-lg">
+          Log in
+        </button>
       </div>
     </div>
   );
 };
 
-export default SignUp;
+export default LogIn;
