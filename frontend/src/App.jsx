2786213547a186bb6dad3a2f8cca2419cc6812b9
Ezeigbo Emmanuel <ezeigboemmanuel2@gmail.com>
2025-01-16 11:29:16 +0100

    Fetchuser implement successfully

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,8 +5,19 @@ import Footer from "./components/Footer";
 import SignUp from "./pages/SignUp";
 import LogIn from "./pages/LogIn";
 import { Toaster } from "react-hot-toast";
+import { useAuthStore } from "./store/authStore";
+import { useEffect } from "react";
 
 function App() {
+  const { fetchUser, fetchingUser } = useAuthStore();
+
+  useEffect(() => {
+    fetchUser();
+  }, [fetchUser]);
+
+  if (fetchingUser) {
+    return <p>Loading...</p>;
+  }
   return (
     <>
       <Toaster position="top-right" />
