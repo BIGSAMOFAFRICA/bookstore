4a19014d79d0bc9c62e11068550810b1f02a4181
Ezeigbo Emmanuel <ezeigboemmanuel2@gmail.com>
2025-01-18 16:01:43 +0100

    Update: Footer text

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
     <div className="text-[#252422] bg-[#CCC5B9] px-4 md:px-12 md:text-lg">
       <h3 className="border-t border-[#252422] pt-4 pb-6 italic">
-        Built with the MERN stack by{" "}
+        Designed and developed by{" "}
         <Link to={"https://zgboportfolio.vercel.app"} target="_blank" className="text-[#944424]">
           Emmanuel Ezeigbo.
         </Link>
