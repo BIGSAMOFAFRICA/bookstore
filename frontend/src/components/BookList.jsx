51cdb5520021407f8377adb5a3c76e4fedd52feb
Ezeigbo Emmanuel <ezeigboemmanuel2@gmail.com>
2025-01-18 15:59:15 +0100

    Fetched books successfuly

--- frontend/src/components/BookList.jsx
+++ frontend/src/components/BookList.jsx
@@ -4,12 +4,11 @@ import { useBookStore } from "../store/bookStore";
 const BookList = () => {
   const { books, fetchBooks } = useBookStore();
 
-  console.log("Books: ", books);
-  const booklist = ["", "", "", "", "", ""];
-
   useEffect(() => {
     fetchBooks();
   }, [fetchBooks]);
+
+
   return (
     <div className="text-[#252422] bg-[#CCC5B9] px-4 md:px-12 pb-20">
       <h1 className="py-6 text-xl md:text-2xl lg:text-3xl w-full mx-auto max-w-6xl">
@@ -17,18 +16,20 @@ const BookList = () => {
       </h1>
 
       <div className="flex flex-wrap justify-around space-x-2 gap-y-5 lg:gap-y-8 w-full max-w-6xl mx-auto">
-        {booklist.map((book, index) => (
+        {books.map((book, index) => (
           <div
             key={index}
             className="cursor-pointer w-[9rem] md:w-[10.5rem] xl:w-[11rem] shadow-sm hover:shadow-md rounded-b-md"
           >
-            <div className="min-w-[9rem] md:min-w-[10.5rem] xl:min-w-[11rem] h-[12rem] md:h-[13.5rem] lg:h-[13rem] xl:h-[15rem] bg-[#252422]"></div>
+            <div className="min-w-[9rem] md:min-w-[10.5rem] xl:min-w-[11rem] h-[12rem] md:h-[13.5rem] lg:h-[13rem] xl:h-[15rem] bg-[#252422]">
+              <img src={book.image} alt="book_img" className="min-w-[9rem] md:min-w-[10.5rem] xl:min-w-[11rem] h-[12rem] md:h-[13.5rem] lg:h-[13rem] xl:h-[15rem] object-cover object-center" />
+            </div>
 
             <div className="p-2 flex flex-col">
               <h2 className="flex-1 mb-2 font-semibold text-base md:text-lg">
-                Across the River and Into the Trees
+                {book.title}
               </h2>
-              <p className="text-sm md:text-base">Liev Schreiber</p>
+              <p className="text-sm md:text-base">{book.author}</p>
             </div>
           </div>
         ))}
