c35109c13ec58ab02a6806eb735ac9c1cdd70f33
Ezeigbo Emmanuel <ezeigboemmanuel2@gmail.com>
2025-01-18 16:28:10 +0100

    fetchBook FE store done

--- frontend/src/store/bookStore.js
+++ frontend/src/store/bookStore.js
@@ -55,4 +55,20 @@ export const useBookStore = create((set) => ({
       throw error;
     }
   },
+
+  fetchBook: async (id) => {
+    set({ isLoading: true, error: null });
+
+    try {
+      const response = await axios.get(`${API_URL}/fetch-user/${id}`);
+
+      set({ book: response.data.user, isLoading: false });
+    } catch (error) {
+      set({
+        isLoading: false,
+        error: error.response.data.message || "Error adding book.",
+      });
+      throw error;
+    }
+  },
 }));
