e676797a592c91c5eb3f6e2e1b899c13f442dcb7
BIGSAMOFAFRICA <olabisisamuelayomide@gmail.com>
2025-05-26 17:04:23 +0100

    update

--- frontend/src/store/authStore.js
+++ frontend/src/store/authStore.js
@@ -1,7 +1,7 @@
 import { create } from "zustand";
 import axios from "axios";
 
-const API_URL = "https://bookstore-2-osnr.onrender.com";
+const API_URL = "https://bookstore-2-osnr.onrender.com/api";
 axios.defaults.withCredentials = true;
 
 export const useAuthStore = create((set) => ({
