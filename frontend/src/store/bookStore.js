02cb38741ff3da85336157cb985b34f29712f749
BIGSAMOFAFRICA <olabisisamuelayomide@gmail.com>
2025-05-26 17:16:57 +0100

    bookstore

--- frontend/src/store/bookStore.js
+++ frontend/src/store/bookStore.js
@@ -1,7 +1,7 @@
 import { create } from "zustand";
 import axios from "axios";
 
-const API_URL = "https://favlib.onrender.com/api";
+const API_URL = "https://bookstore-2-osnr.onrender.com/api";
 axios.defaults.withCredentials = true;
 
 export const useBookStore = create((set) => ({
